97d36b8d5e0d8d7b604062ad549a08b8ed0518da
Daniel Sidabutar <aguspermadi94@gmail.com>
2018-10-28 17:20:14 +0700

    Refactoring function to update dice images and adding message alert base on result

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Dicee
 //
-//  Created by Daniel Permadi on 27/10/18.
+//  Created by Daniel Agus Sidabutar on 27/10/18.
 //  Copyright © 2018 Daniel Lab. All rights reserved.
 //
 
@@ -20,19 +20,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        updateDiceImages()
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+        
+        let messageUser = "Your first number \(randomDiceIndex1+1) and second number \(randomDiceIndex2+1)"
+        
+        let alert = UIAlertController(title: "NIce, You got it!", message: messageUser, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func updateDiceImages() {
+        
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
         
-//        print(randomDiceIndex1)
-        
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
-        
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
-        
     }
     
 }
