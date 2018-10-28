b838beccbba3be8490896f9ab32b1e4236718189
Daniel Sidabutar <daniel.sidabutar@loket.com>
2018-10-28 10:28:19 +0700

    Building UI using story board, linking with View controller, and making random number function

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Daniel Permadi on 27/10/18.
+//  Copyright © 2018 Daniel Lab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        print(randomDiceIndex1)
+    }
+    
+}
+
